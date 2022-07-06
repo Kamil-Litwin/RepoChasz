bfde06f162d59511af786f125476001232cbf767
lift91 <kamillitwin880@gmail.com>
2022-07-06 08:12:14 +0200

    funkcje cz 1

--- ProjektLekcja1/funkcje część 2/Program.cs
+++ ProjektLekcja1/funkcje część 2/Program.cs
@@ -10,6 +10,22 @@ namespace funkcje_część_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Dodaj(4,4));
+            Console.ReadKey();
+        }
+
+        static string Dodaj(int a, int b)
+        {
+            int wynik = a + b;
+            if (wynik> 10)
+            {
+                return "wiekszy niz 10";
+            }
+            else
+            {
+                return "Wynik mniejszy niz 10";
+            }
+            ;
         }
     }
 }
